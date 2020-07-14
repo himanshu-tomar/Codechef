7f1872c5519fe841208b4180f0f3e301b90434c0
Himanshu Tomar <himanshu-tomar@users.noreply.github.com>
2020-07-14 21:48:51 +0530

    incomplete

---
+++ contest/Long Challenge/2018/FEB/CHEFPTNT.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int N,M,X,K;
+        cin>>N>>M>>X>>K;
+        string S;
+        cin>>S;
+
+        
+        
+    }
+    return 0;
+}
