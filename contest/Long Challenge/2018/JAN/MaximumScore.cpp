9f35f8ff1208c6c0f23a415997e440cad066899f
Himanshu Tomar <himanshu-tomar@users.noreply.github.com>
2020-07-14 18:27:23 +0530

    Long Challenge

---
+++ contest/Long Challenge/2018/JAN/MaximumScore.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int N; cin>>N;
+        int A[N][N];
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                cin>>A[i][j];
+            }
+        }
+        for(int i=0;i<N;i++){
+            sort(A[i], A[i]+N);
+        }
+
+        int sum = A[N-1][N-1];
+        int max = A[N-1][N-1];
+        bool flag = true;
+        for(int i=N-2; i>=0; i--){
+            for(int j=N-1;j>=0 && flag ;j--){
+                if(A[i][0] < max){
+                    if(A[i][j] < max ){
+                        sum += A[i][j];
+                        max = A[i][j];
+                        break;
+                    }
+                    else{
+                        continue;
+                    }
+                }
+                else flag = false;
+            }
+            if(!flag){
+                sum = -1;
+                break;
+            }
+        }
+
+        cout<<sum<<endl; 
+        
+    }
+    return 0;
+}
