de4b4489460b787f161f3a2ac6d3502cb7a54e00
Himanshu Tomar <40240028+himanshu-tomar@users.noreply.github.com>
2020-10-04 01:54:59 +0530

    files added

---
+++ contest/Long Challenge/2020/OCT20B/POSAND.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+string str[] = {
+            "-1",
+            "1",
+            "-1",
+            "1 3 2",
+            "-1",
+            "4 5 1 3 2",
+            "6 4 5 1 3 2",
+            "7 6 4 5 1 3 2",
+            "-1",
+            "8 9 7 6 4 5 1 3 2",
+        };
+
+bool isPowerOf2(int n){
+    if(n&1) return false;
+    if(n==2) return true;
+    n/=2;
+    return isPowerOf2(n);
+}
+
+void result(int n){
+    int i=n,current=n;
+    while(i>9){
+        int next = current-1;
+        if(isPowerOf2(next)){
+            cout<<next<<' ';
+            cout<<current<<' ';
+            current-=2;
+            i-=2;
+        }
+        else{
+            cout<<current<<' ';
+            current--;
+            i--;
+        }
+    }
+    cout<<str[9]<<endl;
+}
+
+
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+        cin>>n;
+        if(isPowerOf2(n)) cout<<"-1\n";
+        else if(n<=9){ //subtask 1
+            cout<<str[n]<<endl;
+        }
+        else{
+            result(n);
+        }
+	}
+	return 0;
+}
